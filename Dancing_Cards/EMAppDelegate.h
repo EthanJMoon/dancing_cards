680cc2d7d35548ff903bd580d4e987da8ada8404
Ethan Moon <contact@ethanmoon.com>
2014-05-09 14:36:41 -0400

    Initial Commit

---
+++ Dancing_Cards/EMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  EMAppDelegate.h
+//  Dancing_Cards
+//
+//  Created by Ethan on 5/9/14.
+//  Copyright (c) 2014 EthanMoon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Dancing_Cards/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Dancing_Cards
+//
+//  Created by Ethan on 5/9/14.
+//  Copyright (c) 2014 EthanMoon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "EMAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([EMAppDelegate class]));
+    }
+}
